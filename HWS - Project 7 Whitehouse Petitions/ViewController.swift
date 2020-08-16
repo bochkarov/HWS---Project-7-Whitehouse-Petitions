5779fa5c55c56769e195d86fceec2fc88cc6da7b
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-08-16 11:39:51 +0200

    Initial Commit

---
+++ HWS - Project 7 Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS - Project 7 Whitehouse Petitions
+//
+//  Created by Bochkarov Valentyn on 16/08/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
